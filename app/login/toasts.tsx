5e59bdc6e59c0120f5e432db0e38c56c4d96f49a
yazhou <yazhouhu@yunify.com>
2023-10-31 05:16:32 +0800

    fix: fix toast
    Signed-off-by: yazhou <yazhouhu@yunify.com>

--- app/login/toasts.tsx
+++ app/login/toasts.tsx
@@ -1,46 +1,51 @@
-import { Modal, ModalBody, ModalContent } from '@nextui-org/react'
-import * as React from 'react'
-import { createRoot } from 'react-dom/client'
-
+import { Modal, ModalBody, ModalContent } from "@nextui-org/react";
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import { take, timeout, timer } from "rxjs";
 
 const open = (props: {
-    title: string,
-    content: string,
-    type: 'success' | 'error' | 'info' | 'warning',
+  title: string;
+  content: string;
+  type: "success" | "error" | "info" | "warning";
 }) => {
-    const root = createRoot(document.getElementById('base-modal') as HTMLElement);
-    
-    const Totast = (props: {
-        title: string,
-        content: string,
-        type: 'success' | 'error' | 'info' | 'warning',
-    }) => {
-        const [visible, setVisible] = React.useState(false)
-    
-        React.useEffect(() => {
-            setVisible(true)
-            setTimeout(() => {
-                setVisible(false)
-                root.unmount()
-            }, 3000)
-        }, [])
-        return (
-            <Modal isOpen={visible}
-                placement='top'
-                size='sm'
-            >
-                <ModalContent>
-                    <ModalBody>
-                        <h3 className={'text-xl font-bold'}>{props.title}</h3>
-                        <p className={'text-sm'}>{props.content}</p>
-                    </ModalBody>
-                </ModalContent>
-            </Modal>
-        )
-    }
-    root.render(<Totast {...props} />)
-}
+  const rootDom = document.getElementById("base-modal");
+  if (!rootDom) return;
+  while (rootDom.firstChild) {
+    rootDom.removeChild(rootDom.firstChild);
+  }
+  const root = createRoot(rootDom as HTMLElement);
+
+  const Totast = (props: {
+    title: string;
+    content: string;
+    type: "success" | "error" | "info" | "warning";
+  }) => {
+    const [visible, setVisible] = React.useState(false);
+
+    React.useEffect(() => {
+      setVisible(true);
+      const run = timer(3000)
+        .pipe(take(1))
+        .subscribe(() => {
+          setVisible(false);
+          root.unmount();
+          run.unsubscribe();
+        });
+    }, []);
+    return (
+      <Modal defaultOpen placement="top" size="sm">
+        <ModalContent>
+          <ModalBody>
+            <h3 className={"text-xl font-bold"}>{props.title}</h3>
+            <p className={"text-sm"}>{props.content}</p>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    );
+  };
+  root.render(<Totast {...props} />);
+};
 
 export default {
-    open   
-}
+  open,
+};
